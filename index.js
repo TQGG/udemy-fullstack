5393f088db6a8c1c8110ed95e9da4dd245bb1d43
tianqire <tianqire@usc.edu>
2018-10-26 22:27:26 -0700

    changed greeting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express"); // common js modules - not ES 2015 modules
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ hi: "there" });
+  res.send({ bye: "buddy" });
 });
 
 // app has methods - get, post, put, delete, patch
