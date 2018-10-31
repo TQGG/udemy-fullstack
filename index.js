f74d50b2d7c5f5a93d6eb5e50b4d04e714faa45d
tianqire <tianqire@usc.edu>
2018-10-30 23:31:17 -0700

    finished auth flow

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const express = require("express"); // common js modules - not ES 2015 modules
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const keys = require("./config/keys");
+require("./models/User");
+require("./services/passport");
+
+// connect to mongoDb
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ bye: "buddy" });
-});
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require("./routes/authRoutes")(app);
 
 // app has methods - get, post, put, delete, patch
 const PORT = process.env.PORT || 5000;
